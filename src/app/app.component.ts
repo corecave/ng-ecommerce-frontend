41dce4c6b7eb93930b73b656f5ca96736d7e47bb
Nady Shalaby <nady.shalaby@smart-medicalservices.com>
2021-03-02 16:15:21 +0200

    Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  public ngOnInit() {
+    ajax('http://127.0.0.1:8000/api/products/').subscribe((e) =>
+      console.log(e)
+    );
+  }
 }
